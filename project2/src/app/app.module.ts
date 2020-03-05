fdaf5e9016a926a5f3db8dca037c748f13b31851
clcollins1 <chrislcollins12@gmail.com>
2020-03-04 21:56:00 -0500

    initial restaurant component commit

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
