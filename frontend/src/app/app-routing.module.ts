0645ce28ad9fc74bda046bdc34be377321292194
unknown <mdavis272>
2020-03-07 22:47:17 -0500

    added sign-in and templet for create account

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 // import { NavbarComponent } from '../app/components/navbar/navbar.component';
 import { RestaurantComponent } from './components/restaurant/restaurant.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'rest', component: RestaurantComponent},
-
+  {path: 'signIn', component: SignInComponent},
+  {path: 'createAccount', component: CreateUserComponent}
 ];
 
 
@@ -17,4 +20,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule { }
-export const routingComponents = [ RestaurantComponent, LandingComponent ]
+export const routingComponents = [ RestaurantComponent, LandingComponent ];
