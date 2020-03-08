73099e3575dc36b3459b1e6903f5921f288f3cf9
clcollins1 <chrislcollins12@gmail.com>
2020-03-08 09:37:15 -0400

    retrieved restaurants from api

--- frontend/src/app/components/restaurant/restaurant.component.ts
+++ frontend/src/app/components/restaurant/restaurant.component.ts
@@ -11,124 +11,18 @@ import { RestaurantService } from 'src/app/services/RestaurantService/Restaurant
 })
 export class RestaurantComponent implements OnInit {
 
-    restaurant: string [];
-
-//   restaurant: [{
-//     id: number,
-//     name: string,
-//     category: string [],
-//     deliveryFee: string,
-//     picture: string,
-//     minTime: number,
-//     maxTime: number,
-//     menu: number
-//   },
-//   {
-//     id: number,
-//     name: string,
-//     category: string,
-//     deliveryFee: string,
-//     picture: string,
-//     minTime: number,
-//     maxTime: number,
-//     menu: number
-//   },
-//   {
-//     id: number,
-//     name: string,
-//     category: string,
-//     deliveryFee: string,
-//     picture: string,
-//     minTime: number,
-//     maxTime: number,
-//     menu: number
-//   },
-//   {
-//     id: number,
-//     name: string,
-//     category: string,
-//     deliveryFee: string,
-//     picture: string,
-//     minTime: number,
-//     maxTime: number,
-//     menu: number
-//   }
-// ];
+    restaurant: any [];
 
   constructor(private restaurantService: RestaurantService) { }
- // constructor() {}
 
     ngOnInit(): void {
-    //  this.restaurant = this.getAllRestaurants(this.restaurantService.result);
     this.restaurantService.getRestaurants().subscribe(
-      (data) => this.getAllRestaurants(data),
+      (data) => this.restaurant = data.restaurants,
       (error) => console.log(error)
     );
     }
 
-    getAllRestaurants(restaurant) {
 
-      for (const rest of restaurant.restaurants) {
-        this.restaurant.push(rest);
-      }
-      // console.log(restaurant);
-      // return this.restaurant;
-    }
-
-    // createImgFromBlob(image: Blob) {
-    //   const reader = new FileReader();
-
-    //   reader.addEventListener('load', () => {
-    //     this.img = reader.result;
-    //   }, false);
-    //   if (image) {
-    //     reader.readAsDataURL(image);
-    //   }
-    // }
-  // ngOnInit(): void {
-
-  //   // this.reloadData();
-  //   this.restaurant = [{
-  //     id: 1,
-  //     name: 'Denny\'s',
-  //     category: ['Breakfast'],
-  //     deliveryFee: '2.99',
-  //     picture: 'https://marketingland.com/wp-content/ml-loads/2017/01/Dennys-Diner.jpg',
-  //     minTime: 30,
-  //     maxTime: 45,
-  //     menu: 1
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Mcdonald\'s',
-  //     category: 'Fast Food',
-  //     deliveryFee: 'Free',
-  //     picture: 'https://www.rd.com/wp-content/uploads/2017/12/the-true-story-behind-the-mysterious-mcdonalds-'
-  //     + 'gold-card-673955074-Vytautas-Kielaitis-1024x683.jpg',
-  //     minTime: 30,
-  //     maxTime: 45,
-  //     menu: 2
-  //   },
-  //   {id: 3,
-  //   name: 'Zaxby\'s',
-  //   category: 'Dinner/Lunch',
-  //   deliveryFee: '5.99',
-  //   picture: 'https://www.mississippiweekend.com/wp-content/uploads/2019/02/20190130_125646-1024x768.jpg',
-  //   minTime: 50,
-  //   maxTime: 65,
-  //   menu: 3
-  //   },
-  //   {      id: 4,
-  //     name: 'Yats',
-  //     category: 'Lunch',
-  //     deliveryFee: '2.99',
-  //     picture: 'https://snworksceo.imgix.net/bsd/6199bf62-0b5b-4036-b388-cbdb41c6b53f.sized-1000x1000.JPG?w=1000',
-  //     minTime: 25,
-  //     maxTime: 35,
-  //     menu: 4
-  //   }
-  // ];
-  // }
   // reloadData() {
   //   this.restaurant = this.restaurantService.getRestaurantList();
   // }
--- frontend/src/app/services/RestaurantService/RestaurantService.service.ts
+++ frontend/src/app/services/RestaurantService/RestaurantService.service.ts
@@ -10,19 +10,19 @@ export class RestaurantService {
 
   // private baseUrl = '';
   private testUrl = 'https://eatstreet.com/publicapi/v1/restaurant'
-  + '/search?method=both&pickup-radius=20&street-address=12702+Bruce+B+Downs+Blvd,+Tampa,+FL+33612';
+  + '/search?access-token=261ad9c0491c92b2&method=both&pickup-radius=20&street-address=12702+Bruce+B+Downs+Blvd,+Tampa,+FL+33612';
 
   private imgUrl = 'https://static.eatstreet.com/assets/images/restaurant_logos/bawarchi-biryani-point-38636_1478555896705.png';
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'X-Access-Token': '261ad9c0491c92b2',
-      'Access-Control-Allow-Origin': 'http://localhost:4200',
-      'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Access-Token, content-type',
-      'Access-Control-Allow-Methods': '*'
-    })
-  };
+  // private httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     'x-access-token': '261ad9c0491c92b2'
+      // 'Access-Control-Allow-Origin': 'http://localhost:4200',
+      // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Access-Token, content-type',
+
+  //   })
+  // };
 
   // private httpHeaders = new HttpHeaders().append('X-Access-Token', '261ad9c0491c92b2').set('Content-Type', 'application/json');
 
@@ -32,7 +32,7 @@ export class RestaurantService {
   constructor(private http: HttpClient) { }
 
   getRestaurants(): Observable<any> {
-    return this.http.get(this.testUrl, this.httpOptions);
+    return this.http.get(this.testUrl);
   }
   // result = from(fetch(this.testUrl, {
   //   headers: {
