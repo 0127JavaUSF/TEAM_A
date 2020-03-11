f63db2a0a8a306f74727ad8cf5f684eaceef5fdd
unknown <mdavis272>
2020-03-10 22:11:13 -0400

    added signIn and create account ts

--- frontend/src/app/components/create-user/create-user.component.ts
+++ frontend/src/app/components/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { userInfo } from 'os';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/user';
 
 @Component({
   selector: 'app-create-user',
@@ -9,24 +10,43 @@ import { Component, OnInit } from '@angular/core';
 
 export class CreateUserComponent implements OnInit {
 
-  User: User = new User();
+  user = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  phoneNumber: '',
+  address: '',
+  city: '',
+  state: '',
+  zipCode: ''
+};
 
-  constructor() { }
+confirmPassword = '';
+  notMatch = true;
+
+  error = 'Passwords do not match';
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
+  submit() {
 
+    this.notMatch = this.confirmPass(this.user.password, this.confirmPassword);
+
+    if (this.notMatch === true) {
+      return this.error;
+    }
+    return this.httpClient.post('localhost:9010/user', this.user);
+  }
+
+  confirmPass(password, confirmPassword) {
+    if (password === password) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
-export class User {
-  firstName = '';
-  lastName = '';
-  email = '';
-  password = '';
-  confirmPassword = '';
-  phoneNumber = '';
-  address = '';
-  city = '';
-  state = '';
-  zipCode = '';
-}
