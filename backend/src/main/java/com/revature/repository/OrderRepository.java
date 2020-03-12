1559299630a3d945ea8b69c25a2f2a68abc6f89c
clcollins1 <chrislcollins12@gmail.com>
2020-03-12 13:21:54 -0400

    added controller to return orders for user

--- backend/src/main/java/com/revature/repository/OrderRepository.java
+++ backend/src/main/java/com/revature/repository/OrderRepository.java
@@ -1,12 +1,17 @@
 package com.revature.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.model.Order;
+import com.revature.model.User;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long>{	
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+	Optional<Order> findByUser(User user);	
 	
 
 }
