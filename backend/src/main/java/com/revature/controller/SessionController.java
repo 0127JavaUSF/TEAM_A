138a2d9e26091f59f8d59ea33902e3c539288d56
anvar <antnotes7@gmail.com>
2020-03-11 21:40:17 -0400

    add SessionController

--- backend/src/main/java/com/revature/controller/SessionController.java
+++ backend/src/main/java/com/revature/controller/SessionController.java
@@ -4,14 +4,15 @@
 
 package com.revature.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session.Cookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,48 +25,77 @@ import org.springframework.web.bind.annotation.RestController;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.revature.model.User;
+import com.revature.service.SessionService;
 import com.revature.service.UserService;
 
 import javassist.NotFoundException;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://localhost:4200", allowedHeaders = "*")
 @RequestMapping("/login")
 public class SessionController {
 
 	@Autowired
 	private UserService userServ;
 	
-	@PostMapping(consumes = "application/json", produces = "application/json")
+	@Autowired
+	private SessionService sessServ;
+	
+	@PostMapping(produces = "application/json")
 	public ResponseEntity<String> createSession(String email, String password, 
 			HttpServletRequest request, HttpServletResponse response)
 			throws NotFoundException {
-		
+
 		String token = "";
 		
-		Optional<User> user = userServ.getUserByEmail(email);
-		if(user.isPresent()) {
-			if(BCrypt.checkpw(password, user.get().getPassword())) {
-				// login user here. Meaning assign them a cool token
-				Algorithm algo = Algorithm.HMAC256(user.get().getFirstName());
-				/**
-				 * Need to be returned as part of response header in the cookie
-				 */
-				token = JWT.create()
-						.withClaim("email", email)
-						.withClaim("password", password)
-						.withIssuer("auth0")
-						.sign(algo);
-			};
+		// does user have a token?
+		
+		String userToken = sessServ.extractAuthToken(request);
+		
+		Map<String, String> result = new HashMap<String, String>();
+				
+		if(userToken != null) {
+			if(sessServ.isAuthenticated(email, userToken)) {
+				// if user has cookie and they are authenticated
+				// then all good.
+				return new ResponseEntity<String>("Success", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("Failure", HttpStatus.UNAUTHORIZED);
+			}
+		} else {
+					
+			Optional<User> user = userServ.getUserByEmail(email);
 			
-//			Cookie cookie = new Cookie();
-//			// add this to the header
-//			cookie.setName("somethin");
-//			cookie.setComment("another somethin");
-//			response.addCookie();
+			if(user.isPresent()) {
+					
+				String hashedPwd = user.get().getPassword();
+				
+				if(sessServ.isPassword(password, hashedPwd)) {
+					
+					Algorithm algo = Algorithm.HMAC256(user.get().getFirstName());
+					/**
+					 * Need to be returned as part of response header in the cookie
+					 */
+					token = JWT.create()
+							.withClaim("email", email)
+							.withClaim("password", password)
+							.withIssuer("auth0")
+							.sign(algo);
+	
+					Cookie cookie = new Cookie("auth_token", token);
+					response.addCookie(cookie);
+					return new ResponseEntity<String>("Success. Cookie added.", HttpStatus.OK);
+
+				}
+				
+				return new ResponseEntity<String>("Failure", HttpStatus.UNAUTHORIZED);
+				
+			}
 			
+			return new ResponseEntity<String>("Failure", HttpStatus.NOT_FOUND);
+						
 		}
-		return new ResponseEntity<String>(token, HttpStatus.OK);
+				
 	}
 	
 	@GetMapping("/{id}")
--- backend/src/main/java/com/revature/model/Order.java
+++ backend/src/main/java/com/revature/model/Order.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "order")
+@Table(name = "order_table")
 public class Order {
 	
 	
