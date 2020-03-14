71ed60895bc66aae29c87599c5ca89361650362b
anvar <antnotes7@gmail.com>
2020-03-14 00:18:55 -0400

    fix merge

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../user';
 import { Observable } from 'rxjs';
-// import { SignInComponent } from '../components/sign-in/sign-in.component';
+import { SignInComponent } from '../components/sign-in/sign-in.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  presignedUrlUser;
   email;
 
   user = new User();
@@ -25,7 +25,7 @@ export class UserService {
     return this.http.post<User>('http://localhost:9010/login?email=' + email + '&password=' + password, {
     email, password}).subscribe(
       data => {
-        this.email = data.email
+        return data;
         // Anvar testing
         // localStorage.setItem('cart', "");
       },
@@ -36,5 +36,27 @@ export class UserService {
   setUser(email) {
     this.email = email;
   }
+
+  updatePassword(user){
+    this.http.post('localhost:9010/user/updatePassword', user).subscribe(
+    (data) => {
+      user = data;
+    },
+    (error) => console.log(error)
+    );
+  }
+
+
+    submitPicture(user, file){
+   this.http.post('localhost:9010/user/', user).subscribe(
+     (data) => {
+       this.presignedUrlUser = data;
+     },
+     (error) => console.log(error)
+   );
+
+   this.http.put(this.presignedUrlUser.presignedUrl,file);
+  }
+
 
 }
